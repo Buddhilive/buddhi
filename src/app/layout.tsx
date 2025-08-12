294781b78b8df7212596c8d00485c13f19683602
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-08-12 22:14:42 +0530

    Remove flowbite and migrate to shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeModeScript } from 'flowbite-react';
-import NavbarComponent from "./components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,12 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <ThemeModeScript />
+        
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavbarComponent />
         {children}
       </body>
     </html>
