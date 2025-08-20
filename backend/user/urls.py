2aa8d725d4e5b850ca2de3ed79ceaf4fdcc5d7ad
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-08-20 23:26:05 +0530

    Add token refresh endpoint

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserInfoView, UserLogoutView, UserRegistrationView, UserLoginView
+from .views import UserInfoView, UserLogoutView, UserRegistrationView, UserLoginView, TokenRefreshView
 
 urlpatterns = [
     path('info/', UserInfoView.as_view(), name='user-info'),
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('logout/', UserLogoutView.as_view(), name='user-logout'),
+    path('token-refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -10,7 +10,8 @@ from .serializer import (
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.exceptions import InvalidToken
 
 class UserInfoView(RetrieveUpdateAPIView):
     permission_classes = (IsAuthenticated,)
@@ -80,3 +81,36 @@ class UserLogoutView(APIView):
         response.delete_cookie("access_token")
         response.delete_cookie("refresh_token")
         return response
+
+class TokenRefreshView(TokenRefreshView):
+    def post(self, request):
+        refresh_token = request.COOKIES.get("refresh_token")
+        
+        if not refresh_token:
+            return Response(
+                {"error": "Refresh token not found"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+            
+        try:
+            refresh = RefreshToken(refresh_token)
+            access_token = str(refresh.access_token)
+
+            response = Response(
+                {
+                    "message": "Token refreshed successfully",
+                    "status": status.HTTP_200_OK
+                }
+            )
+            response.set_cookie(
+                key="access_token",
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite="Strict",
+            )
+            return response
+        except InvalidToken:
+            return Response(
+                {"error": "Failed to refresh token. Invalid token."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
