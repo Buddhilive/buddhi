8557c934e21f3d2462058e6cf10c1adb4ad40da6
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-08-29 23:29:34 +0530

    Add basic database model

---
+++ backend/api/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    name = Column(String)
+    age = Column(Integer)
