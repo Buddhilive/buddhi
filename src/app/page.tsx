c04cf492a9f14e53249cf074ec88bbe2a5eda049
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-09-25 09:56:11 +0530

    Connect Tauri app with Python sidecar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,251 @@
+"use client"
+// Need "use client" only if you want this component to use react state. Otherwise, put state logic in child components and mark them "use client".
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { listen } from '@tauri-apps/api/event';
+import { invoke } from '@tauri-apps/api/core';
+// import { Command } from '@tauri-apps/api/shell'
+// import { appWindow } from '@tauri-apps/api/window'
+
+// When using the Tauri global script (if not using the npm package)
+// Be sure to set `app.withGlobalTauri` in `tauri.conf.json` to true
+//
+// const invoke = window.__TAURI__.core.invoke;
+// declare global {
+//   interface Window { __TAURI__: any; }
+// }
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const docs_url = "https://github.com/buddhilive/buddhi";
+  const DOMAIN = "localhost";
+  const PORT = "8008";
+  const [status, setStatus] = useState({ connected: false, info: "" });
+  const [logs, setLogs] = useState("[ui] Listening for sidecar & network logs...");
+  const connectButtonStyle = status.connected ? "hover:border-yellow-300 hover:bg-yellow-100 hover:dark:border-yellow-400 hover:dark:bg-yellow-500/50 border-dashed" : "hover:border-gray-300 hover:bg-gray-100 hover:dark:border-blue-400 hover:dark:bg-blue-500/50";
+  const bgStyle = "bg-[url('/background.svg')] bg-cover bg-fixed bg-center bg-zinc-950";
+  const buttonStyle = "group rounded-lg border border-transparent hover:backdrop-blur px-5 py-4 transition-colors text-left";
+  const greyHoverStyle = "hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30";
+  const descrStyle = "group-hover:opacity-100";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  const initSidecarListeners = async () => {
+    // Listen for stdout lines from the sidecar
+    const unlistenStdout = await listen('sidecar-stdout', (event) => {
+      console.log('Sidecar stdout:', event.payload);
+      if (`${event.payload}`.length > 0 && event.payload !== "\r\n")
+        setLogs(prev => prev += `\n${event.payload}`)
+    });
+
+    // Listen for stderr lines from the sidecar
+    const unlistenStderr = await listen('sidecar-stderr', (event) => {
+      console.error('Sidecar stderr:', event.payload);
+      if (`${event.payload}`.length > 0 && event.payload !== "\r\n")
+        setLogs(prev => prev += `\n${event.payload}`)
+    });
+
+    // Cleanup listeners when not needed
+    return () => {
+      unlistenStdout();
+      unlistenStderr();
+    };
+  }
+
+  const apiAction = async (endpoint: string, method: string = 'GET', payload?: { name: string }) => {
+    const url = `http://${DOMAIN}:${PORT}/${endpoint}`;
+    try {
+      const body = payload ? JSON.stringify(payload) : null;
+      const headers = {
+        "Content-Type": "application/json",
+      };
+
+      const res = await fetch(url, { method, headers, body });
+      if (!res.ok) {
+        throw new Error(`Response status: ${res.status}`);
+      }
+      const json = await res.json();
+      console.log(json);
+      // Success
+      if (json?.message) {
+        setLogs(prev => prev += `\n[server-response] ${json.message}`);
+      }
+      return json;
+    } catch (err) {
+      console.error(`[server-response] ${err}`);
+      setLogs(prev => prev += `\n[server-response] ${err}`);
+    }
+  }
+
+  const connectServerAction = async () => {
+    try {
+      const result = await apiAction("v1/connect");
+      if (result) {
+        setStatus({
+          connected: true,
+          info: `Host: ${result.data.host}\nProcess id: ${result.data.pid}\nDocs: ${result.data.host}/docs`,
+        });
+      }
+      return;
+    } catch (err) {
+      console.error(`[ui] Failed to connect to api server. ${err}`);
+    }
+  }
+
+  const shutdownSidecarAction = async () => {
+    try {
+      const result = await invoke("shutdown_sidecar");
+      if (result) setStatus({
+        connected: false,
+        info: "",
+      });
+      return;
+    } catch (err) {
+      console.error(`[ui] Failed to shutdown sidecar. ${err}`);
+    }
+  }
+
+  const startSidecarAction = async () => {
+    try {
+      await invoke("start_sidecar");
+      return;
+    } catch (err) {
+      console.error(`[ui] Failed to start sidecar. ${err}`);
+    }
+  }
+
+  const mockAPIAction = async () => {
+    try {
+      await apiAction("v1/completions", "POST", { name: "Buddhi" });
+      return;
+    } catch (err) {
+      console.error(`[ui] Failed to get llm completion. ${err}`);
+    }
+  }
+
+  // Start listening for server logs
+  useEffect(() => {
+    initSidecarListeners()
+  }, [])
+
+  // Listen for user key inputs and set full screen.
+  useEffect(() => {
+    const listener = (event: KeyboardEvent) => {
+      if (event.key === 'F11') {
+        event.preventDefault(); // Prevent browser default behavior
+        invoke('toggle_fullscreen');
+      }
+    }
+    window.addEventListener('keydown', listener);
+    // Cleanup
+    return () => {
+      window.removeEventListener('keydown', listener);
+    }
+  }, [])
+
+  return (
+    <main className={`relative flex min-h-screen flex-col items-center justify-between p-24 overflow-hidden ${bgStyle}`}>
+      {/* Spinning Background */}
+      <div className={`absolute flex justify-center items-center left-[50%] right-[50%] bottom-[50%] top-[50%] w-[0px] h-[0px]`}>
+        <div className={`relative w-[100vw] h-[100vw] ${bgStyle} aspect-square animate-[spin_025s_linear_infinite]`}></div>
+      </div>
+      {/* Header/Footer */}
+      <div className="z-20 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        {/* About */}
+        <div className="fixed left-0 top-0 flex flex-col items-center lg:items-start w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <p>
+            Get started by editing&nbsp;
+            <code className="font-mono font-bold text-yellow-300">core/main.py</code>
+          </p>
+          <a href={docs_url}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            Read the project docs:&nbsp;
+            <code className="font-mono font-bold text-yellow-300">here</code>
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* Logo */}
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none pointer-events-none">
+          <div
+            className="flex place-items-center gap-2 p-8 lg:p-0"
+          >
+            <Image
+              src="/logos/app-icon.png"
+              alt="App Logo"
+              className="dark"
+              width={64}
+              height={64}
+              priority
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Area displaying logs from server */}
+      <code className="relative flex max-w-[1200px] max-h-96 font-mono font-bold border dark:border-neutral-800 border-gray-300 rounded-lg backdrop-blur-2xl dark:bg-zinc-800/30 bg-neutral-400/30 p-4 mt-4 mb-4 whitespace-pre-wrap overflow-y-auto">{logs}</code>
+
+      <div className="z-10 mb-32 grid lg:mb-0 lg:grid-cols-4 items-start">
+        {/* Connect to server button */}
+        <button
+          className={`${buttonStyle} ${connectButtonStyle}`}
+          disabled={status.connected}
+          onClick={connectServerAction}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            {status.connected ? "Connected " : "Connect to host"}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50 whitespace-pre-wrap ${descrStyle}`}>
+            {status.connected ? status.info : "Establish connection to api server."}
+          </p>
+        </button>
+        {/* Mock api endpoint button */}
+        <button
+          className={`${buttonStyle} ${greyHoverStyle}`}
+          onClick={mockAPIAction}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Mock API{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50  ${descrStyle}`}>
+            Example api server response from mock endpoint.
+          </p>
+        </button>
+        {/* Start sidecar process button */}
+        <button
+          className={`${buttonStyle} hover:border-gray-300 hover:bg-gray-100 hover:dark:border-green-500 hover:dark:bg-green-500/50`}
+          onClick={startSidecarAction}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Start Sidecar{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50  ${descrStyle}`}>
+            Initialize a new sidecar process.
+          </p>
+        </button>
+        {/* Shutdown sidecar process button */}
+        <button
+          className={`${buttonStyle} hover:border-gray-300 hover:bg-gray-100 hover:dark:border-red-500 hover:dark:bg-red-500/50`}
+          onClick={shutdownSidecarAction}
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Stop Sidecar{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50  ${descrStyle}`}>
+            Force close the sidecar process.
+          </p>
+        </button>
+      </div>
+    </main>
   );
 }
