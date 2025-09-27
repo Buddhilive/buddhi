78bbcf77d657171b5e1d2ea6ab1e1524266b9e2b
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-09-27 14:57:44 +0530

    Update UI Layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export"
+  ...(process.env.NODE_ENV === "production" && {
+    output: "export",
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+  }),
 };
 
 export default nextConfig;
