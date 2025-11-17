1da632ee9fb1647a7cf5568e1d6147f85ff03887
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-11-17 22:30:22 +0530

    Fix the chat

--- src/Home.py
+++ src/Home.py
@@ -10,6 +10,7 @@ LLM_MODEL_NAME = os.path.abspath(os.path.join("models", "gemma-3-270m-it"))
 LLM_SYSTEM_PROMPT = """You are an expert AI assistant providing answers based ONLY on the private documents provided in the context.
 If the answer is not in the documents, state clearly that you cannot answer from the provided information."""
 
+
 @st.cache_resource
 def get_pipeline():
     return pipeline(
@@ -42,10 +43,19 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-# Display assistant response in chat message container
-with st.chat_message("assistant"):
-    stream = client(
-        text_inputs="Hi",
-    )
-    response = st.write_stream(stream)
-st.session_state.messages.append({"role": "assistant", "content": response})
+    messages = [
+        {"role": "system", "content": st.session_state.system_message}
+    ] + [
+        {"role": m["role"], "content": m["content"]} for m in st.session_state.messages
+    ]
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking...", show_time=True):
+            stream = client(
+                text_inputs=messages,
+                max_new_tokens=512,
+                return_full_text=False,
+            )
+            response = st.markdown(stream[0]["generated_text"])
+        st.session_state.messages.append({"role": "assistant", "content": stream[0]["generated_text"]})
